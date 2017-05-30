2177e17f28b07dd89204122da0308a7ead08a31e
Nolan W. Brown <nwbrown@slac.stanford.edu>
2017-05-30 16:59:16 -0700

    added html extension in base template folder

--- web_interface/web_interface/settings.py
+++ web_interface/web_interface/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'web_interface.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates/bases',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
