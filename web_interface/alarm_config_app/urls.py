94fe2b929def56370a4ba8c5531c9eedf0c24751
Nolan W. Brown <nwbrown@slac.stanford.edu>
2017-06-01 14:35:12 -0700

    list, detail views for alerts operational/formatted - linked through list view

--- web_interface/alarm_config_app/urls.py
+++ web_interface/alarm_config_app/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
-    url(r'^debug$', views.list_all, name='list_all'),
+    url(r'^debug/$', views.list_all, name='list_all'),
+    url(r'^title/$', views.title, name='title_page'),
+    url(r'^pvs/$', views.pvs, name='pvs_page'),
+    url(r'^alerts/$', views.alerts, name='alerts_page'),
+    url(r'^pvs_all/$', views.pvs, name='pvs_page_all'),
+    url(r'^alerts_all/$', views.alerts_all.as_view(), name='alerts_page_all'),
+    url(r'^alert_detail/(?P<pk>\d+)/$', views.alert_detail.as_view(), name='alert_detail'),
+
 ]
--- web_interface/alarm_config_app/views.py
+++ web_interface/alarm_config_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-
-#----
+from django.views import generic
 from django.http import HttpResponse
 from .models import Alarm, Pv
 
@@ -15,7 +14,34 @@ def list_all(request):
     context['pv_list'] = Pv.objects.all()
     context['alarm_list'] = Alarm.objects.all()
     context['user'] = request.user
-    return render(request,'debug_list_all.html', context)
-    
+    return render( request, 'debug_list_all.html', context)
     #return HttpResponse("<h1>Page is alive</h1>")
+
+def title(request):
+    context = {}
+    context['user'] = request.user
+    return render( request, 'title.html', context)
+
+def pvs(request):
+    context = {}
+    context['pv_list'] = Pv.objects.all()
+    context['user'] = request.user
+    return render( request, 'pvs.html', context)
+
+def alerts(request):
+    context = {}
+    context['alert_list'] = Alarm.objects.all()
+    context['user'] = request.user
+    return render( request, 'alerts.html', context)
     
+class alerts_all(generic.ListView):
+    model = Alarm
+    template_name = 'alerts_all.html'
+    paginate_by = 10
+
+class alert_detail(generic.DetailView):
+    model = Alarm
+    context_object_name='alert'
+    template_name = 'alert_detail.html'
+
+
