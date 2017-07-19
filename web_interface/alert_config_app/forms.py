7cb3c8edbace31bcf3f235078b640d46c09f75af
rdudschus <rdudschu@ucsc.edu>
2017-07-19 09:04:29 -0700

    Running website sends straight to login. PV view and edit included.

--- web_interface/alert_config_app/forms.py
+++ web_interface/alert_config_app/forms.py
@@ -13,7 +13,7 @@ ____
 """
 
 from django import forms
-from .models import Alert, Pv, Trigger
+from .models import Alert, Pv, Trigger#, PVname
 from account_mgr_app.models import Profile
 
 
@@ -252,6 +252,9 @@ class subscribeAlert(forms.Form):
             data = False
 
         return data
+    
+    
+
 
 class createPv(forms.ModelForm):
     class Meta:
@@ -269,7 +272,7 @@ class createPv(forms.ModelForm):
         )
     )
     # forms.ModelForm.Meta.fields.append(new_name)
-    
+     
 
 
 
