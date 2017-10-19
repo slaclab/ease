7f4ee8eb07bd054539424899cb5c4ec8bb89bcc6
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2017-10-19 13:34:42 -0700

    selenium tests now compatible - require geckodriver installation

---
+++ web_interface/alert_config_app/tests/test_views.py
@@ -0,0 +1,53 @@
+from django.test import TestCase, LiveServerTestCase
+from unittest import skip, skipIf, skipUnless
+import unittest
+import os
+from selenium import webdriver
+#from selenium.webdriver.firefox.webdriver import WebDriver
+# Create your tests here.
+# @skip("Keeping this as an example template")
+
+webdriver_path = os.path.join(os.getcwd(),"geckodriver")
+
+@skipUnless(
+    os.path.isfile(webdriver_path),
+    "Webdriver executable required for UI testing")
+class SeleniumViewTests(LiveServerTestCase):
+    '''I'm keeping this around as a template for other tests
+    '''
+    @classmethod
+    def setUpTestData(cls):
+        #cls.driver = WebDriver()
+        print("setting up test data")
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        
+        cls.wdriver = webdriver.Firefox(executable_path=webdriver_path)
+        # how long to poll for a DOM object before giving up. 
+        cls.wdriver.implicitly_wait(10)
+    
+    def setUp(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        #cls.selenium.quit()
+        cls.wdriver.close()
+
+        super().tearDownClass()
+
+    def test_false_is_false(self):
+        self.assertFalse(False)
+
+    def test_false_is_false(self):
+        self.assertFalse(True)
+
+    def test_one_plus_one_equals_two(self):
+        self.assertEqual(1 + 1, 2)
+
+    def tearDown(self):
+        #Clean up run after every test method.
+        pass
