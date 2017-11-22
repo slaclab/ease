856e89615c5a70960d703146c979b84607ef5dba
omegacore <omegacore@gmail.com>
2017-11-22 00:29:57 -0800

    Got the modal doing queries. Now to put them to use.

--- web_interface/static/pv_search.js
+++ web_interface/static/pv_search.js
@@ -6,7 +6,8 @@ var viewerVars = {};// This is one of the integration points with the server.
 
 // This should default to a path relative location that works from the appliance UI.
 // To develop/debug, override this to a absolute URL of the server with the data you are going to use for debugging/developing.
-viewerVars.serverURL = "https://pswww.slac.stanford.edu/archiveviewer/data";
+viewerVars.serverURL = "https://pswww.slac.stanford.edu/archiveviewer/retrieval";
+viewerVars.serverURL = "http://localhost:8700/archiveviewer/retrieval";//remote work with port forwarding
 
 // User typed a pattern, we search for PV's matching this pattern.
 function searchForPVsMatchingPattern() {
@@ -20,10 +21,11 @@ function searchForPVsMatchingPattern() {
 	}
 	if(pattern) {
 		console.log("Search and add PVs for pattern " + pattern);
+		console.log("URL"+viewerVars.serverURL);
 		var list = $("#pvNameSearchMatchingList");
 		list.empty();
 		$("#pvNameSearchMatchingError").empty();
-		$.getJSON( viewerVars.serverURL + "/../bpl/getMatchingPVs?limit=50&pv=" + pattern, function(matchingPVs){
+		$.getJSON( viewerVars.serverURL + "/bpl/getMatchingPVs?limit=50&pv=" + pattern, function(matchingPVs){
 			if(matchingPVs.length > 0) {
 				matchingPVs.forEach(function(matchingPV) { list.append('<li class="list-group-item">' + matchingPV + '</li>') });
 				$("#pvNameSearchMatchingList li").click(function() { $(this).toggleClass('list-group-item-info'); });
