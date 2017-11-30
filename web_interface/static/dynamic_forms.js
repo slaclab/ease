1aa69779d2061fc1621815392106a67a080f38a0
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2017-11-30 15:23:26 -0800

    progress

--- web_interface/static/dynamic_forms.js
+++ web_interface/static/dynamic_forms.js
@@ -4,25 +4,74 @@
 
 
 class df_controller{
-    constructor(prefix, target_fields, replacements){
+    constructor(prefix, target_fields, replacements, delete_class){
         this.prefix = prefix;
         this.target_fields = target_fields;
         this.replacements = replacements;
+        this.delete_class = delete_class;
 
         // inform developer of arg length issue
         if (this.target_fields.length != this.replacements.length){
             throw "target_fields and replacments mush have same length";
         }    
+        
+        // this.next_contents_no designates the next no. for the next row.
+        var this.next_contents_no = $('input[name=tg-TOTAL_FORMS]').val();
     }
 
-    other_function(){
-        console.log("other function run succesfully")
+    add_trigger_actions(){
+        
+        var new_contents = $("#contents_section").children().last().clone();
+    
+        // Scan through input fields in row, update fields with new names, IDs
+        for (var i = 0; i < dfc_fields.length; i++){
+            // construct search term to find correct input element
+            find = "[id*=id_"+this.prefix+"-][id*=-"+this.target_fields[i]+"]"
+
+            // seek correct input element   
+            var target_element = new_contents.find(find)
+            
+            // construct new ID
+            update_id = "id_"+this.prefix+
+                "-"+this.next_contents_no.toString()+
+                "-"+this.target_fields[i]
+            
+            // update correct input element with new ID
+            target_element.attr('id', update_id);
+
+            // construct new name
+            update_name = dfc_prefix+
+                "-"+next_contents_no.toString()+
+                "-"+dfc_fields[i]
+
+            // update correct input element with new name
+            target_element.attr('name', update_name);            
+            
+            // update correct imput element with new value 
+            new_contents.find(find).val(dfc_replace[i]);
+        }
+    
+        // Push new row into live html
+        $("#contents_section").append(new_contents);
+
+        var cache = $("input[name=tg-TOTAL_FORMS]").val();
+        console.log(cache);
+        $("input[name=tg-TOTAL_FORMS]").val(Number(cache)+1);
+
+        next_contents_no ++;
+        console.log("click2");
+
+
+        new_contents.find(".delete-btn").click(delete_handler);
+    
+    
+    }
+
+    delete_trigger_actions(){
     }
 
     prepare_document(){
-        this.other_function();
         console.log("preparing dfc");
-        console.log("Has this line changed?");
     }
 
 };
