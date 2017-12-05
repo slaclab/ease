cbb4dffbaecece5449c988ffdb6dfb7fc1a4fbc7
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2017-12-05 13:35:40 -0800

    improved df_controller docs, removed hard-coded names

--- web_interface/static/dynamic_forms.js
+++ web_interface/static/dynamic_forms.js
@@ -1,29 +1,67 @@
-
+/**
+ * dynamic_forms.js
+ *
+ * A javascript tool for automating the creation and iteration of Django
+ * formsets.
+ * 
+ * See https://en.wikipedia.org/wiki/JSDoc for documentation guidelines.
+ */
 
 
 
 
 class df_controller{
-    constructor(prefix, target_fields, replacements, delete_class){
-        // take arguments 
+    /**
+     * Construct the dynamic form controller by alerting it to the fields that
+     * need to be iterated 
+     *
+     * @constructor
+     * @param {string} prefix The string proceeding dynamically allocated
+     * fields
+     * @param {array} target_fields An array of strings specifying the IDs of
+     * the input fields to iterate.
+     * @param {array} replacements An array specifying the initial values of the
+     * fields when added to the page via the button
+     * @param {string} form_section The jquery search term for the element
+     * containing the iterated forms. We recommend searching by id using the
+     * '#[id-name]' format.
+     * @param {string} add_button The jquery search term for the button element
+     * that creates more form entries when clicked. We recommend searching by
+     * id using the '#[id-name]' format.
+     * @param {string} delete_button The jquery search term for a button in the
+     * row that deletes the row. We recommend searching by class using the
+     * '.[class-name]' format.
+     * @this {df_controller}
+     */
+    constructor(prefix, target_fields, replacements, form_section, add_button, delete_button){
+        
+        // input arguments 
         this.prefix = prefix;
         this.target_fields = target_fields;
         this.replacements = replacements;
-        this.delete_class = delete_class;
-
+        this.form_section = form_section;
+        this.add_button = add_button;
+        this.delete_button = delete_button;
         // inform developer of arg length issue
         if (this.target_fields.length != this.replacements.length){
             throw "target_fields and replacments mush have same length";
         }    
         
         // this.next_contents_no designates the next no. for the next row.
-        this.next_contents_no = $('input[name=tg-TOTAL_FORMS]').val();
+        this.next_contents_no = $("input[name="+this.prefix+"-TOTAL_FORMS]").val();
     }
 
+    /**
+     * A collection of actions to be completed when the 'add' button is 
+     * pressed. Note that unless the .bind method is used when called @this
+     * will default to the button element instead of the df_controller
+     * instance.
+     *
+     * @this {df_controller}
+     */
     add_trigger_actions(){
         // Collection of actions to take when the "add" button is pressed
-
-        var new_contents = $("#contents_section").children().last().clone();
+        var new_contents = $(this.form_section).children().last().clone();
 
         // Scan through input fields in row, update fields with new names, IDs
         for (var i = 0; i < this.target_fields.length; i++){
@@ -49,52 +87,55 @@ class df_controller{
             // update correct input element with new name
             target_element.attr('name', update_name);            
             
-            // update correct imput element with new value 
+            // update correct input element with new value 
             new_contents.find(find).val(this.replacements[i]);
         }
     
         // Push new row into live html
-        $("#contents_section").append(new_contents);
+        $(this.form_section).append(new_contents);
 
-        var cache = $("input[name=tg-TOTAL_FORMS]").val();
-        console.log(cache);
-        $("input[name=tg-TOTAL_FORMS]").val(Number(cache)+1);
+        var cache = $("input[name="+this.prefix+"-TOTAL_FORMS]").val();
+        $("input[name="+this.prefix+"-TOTAL_FORMS]").val(Number(cache)+1);
 
         this.next_contents_no ++;
 
-        new_contents.find(".delete-btn").click(
-            //this.delete_trigger_actions.bind(this)
-            this.delete_trigger_actions
-        );
-    
+        new_contents.find(this.delete_button).click(
+            this.delete_trigger_actions.bind(this)
+        ); 
     
     }
-
-    delete_trigger_actions(){
+    /**
+     * A collection of actions to be completed when the 'delete' button is
+     * pressed. Note that unless the .bind method is used when calld @this will
+     * default to the button element instead of the df_controller instance.
+     *
+     * @param {event} event this is the event of the button press, it is
+     * automatically sent when called from the click method
+     *
+     *
+     */
+    delete_trigger_actions(event){
         // reject deleting the last remaining entry, there must be at least one
         // trigger at all times
-        if ( $('input[name=tg-TOTAL_FORMS]').val() <= 1 ){
+        if ( $("input[name="+this.prefix+"-TOTAL_FORMS]").val() <= 1 ){
             return;
         }
 
-        $(this).attr('class',"btn btn-danger text-muted")
-        //this.innerHTML = "keep this!";
-        //$(this).parent().parent().remove();
-        $(this).parentsUntil("tbody").last().remove();
-        console.log("delete pressed")
-        //console.log($(this).parent().parent());
-        //console.log($(this).parentsUntil("tbody").last());
-        //console.log($(this)[0].innerHTML); 
-        
-        
-        var cache = $("input[name=tg-TOTAL_FORMS]").val();
-        $("input[name=tg-TOTAL_FORMS]").val(Number(cache)-1);
+        var button = event.target
+
+        $(button).attr('class',"btn btn-danger text-muted")
+        $(button).parentsUntil(this.form_section).last().remove();
+          
+        var cache = $("input[name="+this.prefix+"-TOTAL_FORMS]").val();
+        $("input[name="+this.prefix+"-TOTAL_FORMS]").val(Number(cache)-1);
     }
 
     prepare_document(){
-        $("#add_trigger_btn").click(this.add_trigger_actions.bind(this));
+        $(this.add_button).click(this.add_trigger_actions.bind(this));
         
-        $(".delete-btn").click(this.delete_trigger_actions);
+        $(this.delete_button).click(
+            this.delete_trigger_actions.bind(this)
+        );
     }
 
 };
