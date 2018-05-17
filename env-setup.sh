50fca44834bf296d143ccf8f95560a4fd844a3d7
Wallace <awallace@slac.stanford.edu>
2018-05-17 10:38:47 -0700

    fiiix it

--- env-setup.sh
+++ env-setup.sh
@@ -8,10 +8,10 @@
 if [ -z "$1" ]; then
     echo usage: $0 provisioned file directory?
     echo "using ."
-    1="."
+    TOP_LOC="."
+else
+    TOP_LOC="$1"
 fi
-
-TOP_LOC="$1"
 
 wget "https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
